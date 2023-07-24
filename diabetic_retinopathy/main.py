a6dbaaac056437452ad4c4af69fa54181853fd58
Wenwu Tang <117169041+TWWinde@users.noreply.github.com>
2023-07-24 13:43:37 +0200

    Update main.py

--- diabetic_retinopathy/main.py
+++ diabetic_retinopathy/main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import random
 import gin
 import logging
@@ -17,6 +18,15 @@ from models_Resnet.architectures import resnet
 from models.TL import tl_inception, tl_xception, tl_inception_resnet
 from evaluation.metrics import confusionmatrix, ROC
 
+parser = argparse.ArgumentParser(description='Train model')
+parser.add_argument('--model', choices=['vgg_like', 'resnet', 'tl_inception', 'tl_xception', 'tl_inception_resnet'], default = 'vgg_like'
+                    help='choose model')
+parser.add_argument('--mode', choices=['train','test'], default = 'train', help='train or test')
+parser.add_argument('--evaluation', choices=['evaluate_fl', 'confusionmatrix', 'Dimensionality_Reduction', 'ROC'], default = 'evaluate_fl', help='train or test')
+parser.add_argument('--checkpoint-file', type=str, default='./ckpts/',
+                    help='Path to checkpoint.')
+
+args = parser.parse_args()
 
 # fix the seed to make the training repeatable
 def setup_seed(seed):
@@ -28,19 +38,13 @@ def setup_seed(seed):
 
 setup_seed(66)
 
-FLAGS = flags.FLAGS
-flags.DEFINE_boolean('train', True, 'Specify whether to train or test a model.')  # train
 
 
-FLAG1 = flags.FLAGS
-flags.DEFINE_boolean('TFrecord_needed', False, 'Specify if TFRecord is ready.')
 
 
 @gin.configurable
 def main(argv):
-    # change the number to decide which model.
-    Choose_model = ['vgg_like', 'resnet', 'tl_inception', 'tl_xception', 'tl_inception_resnet']
-    model_flag = Choose_model[0]
+
 
     # change the number to decide the operation.
     Choose = ['evaluate_fl', 'confusionmatrix', 'Dimensionality_Reduction', 'ROC']
@@ -56,55 +60,51 @@ def main(argv):
     gin.parse_config_files_and_bindings(['configs/config.gin'], [])
     utils_params.save_config(run_paths['path_gin'], gin.config_str())
 
-    if FLAG1.TFrecord_needed:
-        # get images and labels
-        processing_augmentation_oversampling()
-        # write TFRecord
-        write_Tfrecord()
-    else:
-        pass
+   
 
     # setup pipeline
     ds_train, ds_val, ds_test, ds_info = load()
 
-    if model_flag == 'vgg_like':
+    if args.model == 'vgg_like':
         model = vgg_like()
-    elif model_flag == 'resnet':
+    elif args.model == 'resnet':
         model = resnet(input_shape=ds_info.input_shape, n_classes=ds_info.n_classes)
-    elif model_flag == 'tl_inception':
+    elif args.model == 'tl_inception':
         model = tl_inception(input_shape=ds_info.input_shape, n_classes=ds_info.n_classes)
-    elif model_flag == 'tl_xception':
+    elif args.model == 'tl_xception':
         model = tl_xception(input_shape=ds_info.input_shape, n_classes=ds_info.n_classes)
-    elif model_flag == 'tl_inception_resnet':
+    elif args.model == 'tl_inception_resnet':
         model = tl_inception_resnet(input_shape=ds_info.input_shape, n_classes=ds_info.n_classes)
+    else:
+        print('Error, model does not exist')
 
     model.summary()
 
-    if FLAGS.train:
+    if args.mode == 'train':
         trainer = Trainer(model, ds_train, ds_val, ds_info, run_paths)
         for _ in trainer.train():
             continue
     else:
         checkpoint = tf.train.Checkpoint(step=tf.Variable(0), model=model)
         manager = tf.train.CheckpointManager(checkpoint,
-                                             directory="/misc/home/RUS_CIP/st180408/dl-lab-22w-team08/experiments/run_2023-02-10T04-06-41-658852/ckpts/",
+                                             directory=args.checkpoint_file,
                                              max_to_keep=3)
 
 
         checkpoint.restore(manager.latest_checkpoint)
         if manager.latest_checkpoint:
-            tf.print("restore")
+            tf.print("restored")
         else:
-            tf.print("bad")
+            tf.print("Error")
 
-        if test_flag == 'evaluate_fl':
+        if args.evaluation == 'evaluate_fl':
             ds_test = ds_test.batch(32)
             evaluate_fl(model, ds_test)
-        elif test_flag == 'confusionmatrix':
+        elif args.evaluation == 'confusionmatrix':
             confusionmatrix(model, ds_test)
-        elif test_flag == 'Dimensionality_Reduction':
+        elif args.evaluation == 'Dimensionality_Reduction':
             Dimensionality_Reduction(model, ds_test)
-        elif test_flag == 'ROC':
+        elif args.evaluation == 'ROC':
             ROC(model, ds_test)
 
 
