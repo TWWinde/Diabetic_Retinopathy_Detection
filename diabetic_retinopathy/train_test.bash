80dedf71e8ec5321581997f7e60b2afa77822b59
Wenwu Tang <117169041+TWWinde@users.noreply.github.com>
2023-07-24 20:28:46 +0200

    Update train_test.bash

--- diabetic_retinopathy/train_test.bash
+++ diabetic_retinopathy/train_test.bash
@@ -3,11 +3,11 @@
 CHECKPOINT_FILE="/projects/tang/fsg/preprocess/outputs/MPIIGaze.h5"
 
 CMD=""
-	  CMD="main.py \
-		--checkpoint-file ${CHECKPOINT_FILE} \
-  	--mode train \
-		--model vgg-like \
-    __evaluation ROC\
-        "
+CMD="main.py \
+    --checkpoint-file ${CHECKPOINT_FILE} \
+    --mode train \
+    --model vgg-like \
+    __evaluation ROC\"
+    
     eval "python $CMD "
     
